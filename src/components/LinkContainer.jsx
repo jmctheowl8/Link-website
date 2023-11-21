eebc0d74c0cc2d6897c32c7f4b674c2a20015b6f
jmctheowl8 <john.cabrera@lc.cuny.edu>
2023-11-20 21:24:42 -0500

    checking if commit works

--- src/components/LinkContainer.jsx
+++ src/components/LinkContainer.jsx
@@ -18,6 +18,7 @@ function LinkContainer(){
     return(
         <div>
             <h1>My Favorite Links</h1>
+            <h1>Change test</h1>
             <p>Add a new link with a name and URL to the table! </p>
             <Table />
             <h1>Add New</h1>
